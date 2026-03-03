8001c3b2533a6ae76da481009994b3e9fe199979
Colin <colin.diesh@gmail.com>
2026-03-03 12:03:38 -0500

    Update deps

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -166,7 +166,7 @@ function toPascalCase(string: string) {
       ($1, $2, $3) => `${$2.toUpperCase() + $3.toLowerCase()}`,
     )
     .replace(new RegExp(/\s/, 'g'), '')
-    .replace(new RegExp(/\w/), s => s.toUpperCase())
+    .replace(new RegExp(/\w/), (s) => s.toUpperCase())
 }
 
 function getSafePackageName(name: string) {
